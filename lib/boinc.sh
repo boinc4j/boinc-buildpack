e1a59f185c3404ce3576f04fd6929691d92cbed6
Joe Kutner <jpkutner@gmail.com>
2015-12-08 17:58:08 -0600

    Removed default project in favor of user provided project

--- lib/boinc.sh
+++ lib/boinc.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+DEFAULT_PLATFORMS="windows_intel86 windows_x86_64 i686-pc-linux-gnu x86_64-pc-linux-gnu i686-apple-darwin x86_64-apple-darwin"
+
 install_boinc() {
   #### Download BOINC source
   echo "-----> Downloading BOINC source... "
@@ -22,6 +24,60 @@ add_project_xml() {
 
   sed -i.bak s/example_app/${projectName}/g ${projectXml}
   sed -i.bak s/Example\ Application/${projectName}/g ${projectXml}
+}
+
+add_wrapper_bin() {
+  local appDir=${1}
+  local platform=${2}
+  local wrapperVersion="26014"
+  local platformDir=${appDir}/${platform}
+
+  curl -O -s -L http://boinc.berkeley.edu/dl/wrapper_${wrapperVersion}_${platform}.zip
+  unzip wrapper_${wrapperVersion}_${platform}.zip
+  mkdir -p ${platformDir}
+  mv wrapper_${wrapperVersion}_${platform} ${platformDir}/wrapper_${wrapperVersion}_${platform}
+  rm -f wrapper_${wrapperVersion}_${platform}.zip
+
+  sed -i.bak -e "s/<\/version>//g" ${platformDir}/version.xml
+  cat <<EOF >> ${platformDir}/version.xml
+   <file>
+      <physical_name>wrapper_${wrapperVersion}_${platform}</physical_name>
+      <main_program/>
+      <copy_file/>
+   </file>
+   <is_wrapper/>
+</version>
+EOF
+}
+
+add_app_files() {
+    local targetFiles=${1}
+    local appDir=${2}
+
+    if [ -f version.xml ] && grep -q "<main_program/>" version.xml; then
+      # huh?
+      echo "Non-wrapper apps are not yet supported"
+      exit 1
+    elif [ -f version.xml ]; then
+      for platform in $PLATFORMS; do
+        mv $targetFiles/$platform $appDir/$platform
+        app_wrapper_bins $appDir $platform
+      done
+    else
+      echo "ERROR: no version.xml in $platform"
+      exit 1
+    fi
+}
+
+sign_files() {
+  local boincDir=${1}
+  local appDir=${2}
+
+  for platformDir in $(ls ${appDir}); do
+    for file in $(find ${appDir}/${platformDir} -type f -follow -print); do
+      $boincDir/lib/crypt_prog -sign $file keys/code_sign_private > $file.sig
+    done
+  done
 }
 
 create_start_script() {
