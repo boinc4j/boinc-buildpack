cd6c71669d4a4ae57c0a4d336f99358cb6dfe486
Joe Kutner <jpkutner@gmail.com>
2015-12-09 15:18:53 -0600

    Fixed a bug in apache setup

--- lib/apache2.sh
+++ lib/apache2.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 apache_enable_cgid() {
-  cat <<EOF >> $BUILD_DIR/project/boinc.httpd.conf
+  cat <<EOF >> $BUILD_DIR/$REL_BOINC_PROJECT_DIR/boinc.httpd.conf
     LoadModule cgid_module /app/.heroku/php/libexec/mod_cgid.so
 EOF
 }
 
 apache_add_index_php() {
-  cat <<EOF >> $BUILD_DIR/project/boinc.httpd.conf
+  cat <<EOF >> $BUILD_DIR/$REL_BOINC_PROJECT_DIR/boinc.httpd.conf
     DirectoryIndex index.php index.html
 EOF
 }
