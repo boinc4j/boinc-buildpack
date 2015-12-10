bf6fc0be8055a8205c22dea1df6cf04bcf7d1284
Joe Kutner <jpkutner@gmail.com>
2015-12-09 19:08:58 -0600

    Added a message

--- lib/boinc.sh
+++ lib/boinc.sh
@@ -31,7 +31,7 @@ make_boinc_project() {
   sed -i.bak s/cursor.execute\(\"create\ database/\#cursor.execute\(\"create\ database/g py/Boinc/database.py
 
   echo "Testing database for existing schema..." | indent
-  mysql -u $DATABASE_USERNAME -p$DATABASE_PASSWORD -h $DATABASE_HOST -e "describe app;" $DATABASE_NAME > /dev/null 2>&1
+  mysql -u $DATABASE_USERNAME -p$DATABASE_PASSWORD -h $DATABASE_HOST -e "describe app;" $DATABASE_NAME #> /dev/null 2>&1
   if [ $? == 0 ]; then
     dbArgs="--no_db"
   fi
