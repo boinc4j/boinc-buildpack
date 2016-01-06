13a652f7da347ac9d6570cb26cebcd6418655977
Joe Kutner <jpkutner@gmail.com>
2016-01-06 16:22:47 -0600

    Tail daemon logs with -f

--- lib/boinc.sh
+++ lib/boinc.sh
@@ -194,7 +194,7 @@ if [ "\$DYNO" = "web.1" ]; then
   sed -i.bak s/\<host\>.*\</\<host\>\$(hostname)\</g config.xml
   bin/start &
   sleep 3
-  tail /app/$relBoincProjectDir/log_\$(hostname)/* &
+  tail -f /app/$relBoincProjectDir/log_\$(hostname)/* &
 fi
 
 cd /app
